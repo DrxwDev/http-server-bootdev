07977fc3294e0fa064be62d08b3fce417c550666
DrxwDev <drxwdev@outlook.es>
2026-05-25 15:36:55 -0400

    serving images with gin.

--- internal/bootdev/controller.go
+++ internal/bootdev/controller.go
@@ -16,3 +16,9 @@ func (c *BootController) Index(ctx *gin.Context) {
 	ctx.File("assets/index.html")
 	ctx.Status(http.StatusOK)
 }
+
+func (c *BootController) Assets(ctx *gin.Context) {
+	ctx.Header("Content-Type", "image/png")
+	ctx.File("assets/logo.png")
+	ctx.Status(http.StatusOK)
+}
--- internal/server/routes.go
+++ internal/server/routes.go
@@ -7,4 +7,5 @@ import (
 
 func Routes(router *gin.Engine, bootdev *bootdev.BootController) {
 	router.GET("/", bootdev.Index)
+	router.GET("/assets/logo.png", bootdev.Assets)
 }
