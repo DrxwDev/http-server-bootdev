4b4e979ea1b4e6232a93973d72060518e97a9578
DrxwDev <drxwdev@outlook.es>
2026-05-25 15:18:52 -0400

    start the server

--- internal/server/routes.go
+++ internal/server/routes.go
@@ -1,13 +1,8 @@
 package server
 
 import (
-	"net/http"
-
 	"github.com/gin-gonic/gin"
 )
 
 func Routes(router *gin.Engine) {
-	router.GET("/", func(ctx *gin.Context) {
-		ctx.JSON(http.StatusOK, gin.H{"status": "ok"})
-	})
 }
